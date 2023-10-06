69d2598f7790eafc3d221c682409aedda9f1945c
rowoon <dev.rowoon@gmail.com>
2023-10-06 11:42:27 +0900

    [core] 의존성 주입 완료

--- src/main/java/upgradeGymCarry/demo/controller/MemberController.java
+++ src/main/java/upgradeGymCarry/demo/controller/MemberController.java
@@ -2,6 +2,7 @@ package upgradeGymCarry.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import upgradeGymCarry.demo.service.MemberService;
 
 @Controller
@@ -11,5 +12,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/login")
+    public String loginForm(){
+        return "members/createLoginForm";
     }
 }
