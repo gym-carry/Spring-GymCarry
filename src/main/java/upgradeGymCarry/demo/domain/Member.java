3c10c8c1268330b7589219aa5945f67e8ff224c5
rowoon <dev.rowoon@gmail.com>
2023-10-06 10:55:43 +0900

    [feat] member 만들었음

--- src/main/java/upgradeGymCarry/demo/domain/Member.java
+++ src/main/java/upgradeGymCarry/demo/domain/Member.java
@@ -1,4 +1,76 @@
 package upgradeGymCarry.demo.domain;
 
+import java.util.Objects;
+
 public class Member {
+    private String id;
+    private String name;
+    private String pwd;
+    private String email;
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Member member = (Member) o;
+        return Objects.equals(id, member.id) && Objects.equals(pwd, member.pwd) && Objects.equals(email, member.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, pwd, email);
+    }
 }
+
